3412a80d09d40966f5efe38b32ecc9365e2ac2e8
songminseok <ske3056@gmail.com>
2023-06-07 00:35:52 +0900

    promise chaining

--- elliecode/async/promise.js
+++ elliecode/async/promise.js
@@ -10,16 +10,43 @@ const promise = new Promise((resolve, reject) => {
   //doing some heavy work(network, read files)
   console.log("doing someing");
   setTimeout(() => {
-    // resolve("ellie");
-    reject(new Error("no Network"));
+    resolve("ellie");
+    // reject(new Error("no Network"));
   }, 2000);
 });
 
 //2. Consumers : then, catch, finally
 promise
   .then((value) => {
+    //성공resolve
     console.log(value);
   })
   .catch((err) => {
+    //실패reject
     console.log(err);
+  })
+  .finally(() => {
+    // 성공여부에 상관없이 실행
+    console.log("finally");
+  });
+
+//3. Promise chaining
+const fetchNumber = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000);
+});
+
+fetchNumber
+  .then((value) => {
+    return value * 2;
+  })
+  .then((value) => {
+    return value * 3;
+  })
+  .then((value) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(value - 1), 1000);
+    });
+  })
+  .then((value) => {
+    console.log(value);
   });
